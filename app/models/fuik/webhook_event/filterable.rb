a3feb0f475b6615d7870d4d569f2b7ac5c4e7a66
eelco <gitt@eelcoj.com>
2026-06-26 11:23:50 +0200

    Removed CONST

--- app/models/fuik/webhook_event/filterable.rb
+++ app/models/fuik/webhook_event/filterable.rb
@@ -7,16 +7,14 @@ module Fuik
 
       class_methods do
         def filtered(params)
-          where(params.permit(*FILTERS).compact_blank.transform_values { it.split(",") })
+          filters = %w[status provider event_id event_type]
+
+          where(params.permit(*filters).compact_blank.transform_values { it.split(",") })
         end
 
         def options_for_select
           by_provider_name.pluck(:provider).map { [it.humanize, it] }
         end
-
-        private
-
-        FILTERS = %w[status provider event_id event_type]
       end
 
       included do
